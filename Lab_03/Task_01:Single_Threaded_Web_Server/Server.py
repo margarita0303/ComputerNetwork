1053d25ea60958e142a902f451a7a1767d2f8065
Margarita <lashina.2000@gmail.com>
2022-03-13 20:27:17 +0300

    added task 1

---
+++ Lab_03/Task_01:Single_Threaded_Web_Server/Server.py
@@ -0,0 +1,39 @@
+import socket
+
+HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+HOST = "127.0.0.1"
+PORT = 1500
+encoding_format = 'utf-8'
+
+
+def start_server():
+    try:
+        server = socket.create_server((HOST, PORT))
+        server.listen(4)
+        while True:
+            conn, address = server.accept()
+            request = conn.recv(1024).decode(encoding_format)
+            conn.send(load_page_from_request(request))
+            conn.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('Server is closed.')
+
+
+def load_page_from_request(request):
+    path = 'Pages' + request.split(' ')[1]
+    try:
+        with open(path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode(encoding_format) + response
+    except FileNotFoundError:
+        return (HDRS_404 + '404 Not Found.').encode(encoding_format)
+
+
+# http://127.0.0.1:1500/home.html
+# http://127.0.0.1:1500/friends.html
+# http://127.0.0.1:1500/news.html
+
+if __name__ == '__main__':
+    start_server()
